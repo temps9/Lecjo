c26c87d2dc7f3d46d8efc93e20dbd6a5964df673
André pierre <temps.jo@gmail.com>
2016-10-24 23:51:23 +0200

    Add files via upload

---
+++ exemple/adn3.js
@@ -0,0 +1,28 @@
+var donne = [1, 220, 100, 0, 50, 2, 0, 0, 0, 30,
+100, 2, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 200, 3, 0,];
+
+
+for(var i= 0; i < 75; i++)
+{
+var nouveauinput;
+function ajouteElemen() {
+// crée un nouvel élément input
+var body   = document.body || document.getElementsByTagName('body')[0],
+nouveauinput  =  document.createElement("input");
+nouveauinput.setAttribute("type", "number");
+nouveauinput.setAttribute("id", ("donne"+i));
+nouveauinput.setAttribute("value", donne[i]);
+nouveauinput.setAttribute("style", "display: none;");
+// ajoute l'élément qui vient d'être créé et son contenu au DOM
+body.insertBefore(nouveauinput,body.childNodes[0]);
+console.log("donne"+i);
+console.log(donne[i]);
+}  
+ajouteElemen();
+}
