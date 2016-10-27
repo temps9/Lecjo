f4e08a9b590e2b3581986783afbd1bbb29597027
André pierre <temps.jo@gmail.com>
2016-10-27 09:34:12 +0200

    Add files via upload

--- exemple/adn4.js
+++ exemple/adn4.js
@@ -1,21 +1,11 @@
-var donne = [1, 220, 100, 0, 0, 0, 0, 0, 0, 30,
-100, 2, 0, 0, 0, 0, 0, 0, 0, 0,
+var donne = [1, 220, 100, 0, 0, 2, 0, 0, 0, 30,
+48, 2, 0, 0, 0, 0, 0, 0, 0, 0,
 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-0, 100, 0, 0, 2];
-
-var donna = [1, 220, 24, 0, 0, 0, 0, 0, 0, 30,
-24, 2, 0, 0, 0, 0, 0, 0, 0, 0,
-0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-0, 100, 0, 0, 2];
-
+0, 100, 3, 0, 1];
 
 for(var i= 0; i < 75; i++)
 {
@@ -36,8 +26,19 @@ console.log(donne[i]);
 ajouteElemen();
 }
 
+/**sound 2***/
 
-for(var i= 0; i < 75; i++)
+donne.splice(1, 73, 220, 12, 0, 0, 2, 0, 0, 0, 30,
+24, 2, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 50, 3, 0);
+
+
+for(var i= 0; i < 74; i++)
 {
 var nouveauinput;
 function ajouteElemen1() {
@@ -45,13 +46,13 @@ function ajouteElemen1() {
 var body   = document.body || document.getElementsByTagName('body')[0],
 nouveauinput  =  document.createElement("input");
 nouveauinput.setAttribute("type", "number");
-nouveauinput.setAttribute("id", ("donna"+i));
-nouveauinput.setAttribute("value", donna[i]);
+nouveauinput.setAttribute("id", ("donne"+i));
+nouveauinput.setAttribute("value", donne[i]);
 nouveauinput.setAttribute("style", "display: none;");
 // ajoute l'élément qui vient d'être créé et son contenu au DOM
 body.insertBefore(nouveauinput,body.childNodes[0]);
-console.log("donna"+i);
-console.log(donna[i]);
+console.log("donne"+i);
+console.log(donne[i]);
 }  
 ajouteElemen1();
 }
