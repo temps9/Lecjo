799909ebefc4e561018dbfe5ffee7677acab37ef
André pierre <temps.jo@gmail.com>
2016-10-29 08:25:37 +0200

    Add files via upload

--- exemple/adn4.js
+++ exemple/adn4.js
@@ -1,11 +1,39 @@
-var donne = [1, 220, 100, 0, 0, 2, 0, 0, 0, 30,
+var donne = [1, 200, 100, 0, 0, 0, 0, 0, 0, 50,
+120, 2, 1, 1, 1, 1, 1, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 30, 0, 0, 4];
+
+var donna = [1, 200, 52, 0, 0, 0, 0, 0, 0, 50,
+20, 2, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 60, 0, 0, 4];
+
+var donnb = [1, 220, 100, 0, 0, 0, 0, 0, 0, 30,
+100, 4, 0, 0, 0, 0, 0, 0, 140, 200,
+0, 0, 0, 0, 0, 0, 0, 110, 200, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 30, 0, 0, 4];
+
+var donnc = [1, 200, 50, 0, 0, 0, 0, 0, 0, 50,
 48, 2, 0, 0, 0, 0, 0, 0, 0, 0,
 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-0, 100, 3, 0, 1];
+0, 60, 0, 0, 4];
+
 
 for(var i= 0; i < 75; i++)
 {
@@ -20,25 +48,12 @@ nouveauinput.setAttribute("value", donne[i]);
 nouveauinput.setAttribute("style", "display: none;");
 // ajoute l'élément qui vient d'être créé et son contenu au DOM
 body.insertBefore(nouveauinput,body.childNodes[0]);
-console.log("donne"+i);
-console.log(donne[i]);
 }  
 ajouteElemen();
 }
 
-/**sound 2***/
 
-donne.splice(1, 73, 220, 12, 0, 0, 2, 0, 0, 0, 30,
-24, 2, 0, 0, 0, 0, 0, 0, 0, 0,
-0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-0, 50, 3, 0);
-
-
-for(var i= 0; i < 74; i++)
+for(var i= 0; i < 75; i++)
 {
 var nouveauinput;
 function ajouteElemen1() {
@@ -46,13 +61,45 @@ function ajouteElemen1() {
 var body   = document.body || document.getElementsByTagName('body')[0],
 nouveauinput  =  document.createElement("input");
 nouveauinput.setAttribute("type", "number");
-nouveauinput.setAttribute("id", ("donne"+i));
-nouveauinput.setAttribute("value", donne[i]);
+nouveauinput.setAttribute("id", ("donna"+i));
+nouveauinput.setAttribute("value", donna[i]);
 nouveauinput.setAttribute("style", "display: none;");
 // ajoute l'élément qui vient d'être créé et son contenu au DOM
 body.insertBefore(nouveauinput,body.childNodes[0]);
-console.log("donne"+i);
-console.log(donne[i]);
 }  
 ajouteElemen1();
+}
+
+for(var i= 0; i < 75; i++)
+{
+var nouveauinput;
+function ajouteElemen2() {
+// crée un nouvel élément input
+var body   = document.body || document.getElementsByTagName('body')[0],
+nouveauinput  =  document.createElement("input");
+nouveauinput.setAttribute("type", "number");
+nouveauinput.setAttribute("id", ("donnb"+i));
+nouveauinput.setAttribute("value", donnb[i]);
+nouveauinput.setAttribute("style", "display: none;");
+// ajoute l'élément qui vient d'être créé et son contenu au DOM
+body.insertBefore(nouveauinput,body.childNodes[0]);
+}  
+ajouteElemen2();
+}
+
+for(var i= 0; i < 75; i++)
+{
+var nouveauinput;
+function ajouteElemen3() {
+// crée un nouvel élément input
+var body   = document.body || document.getElementsByTagName('body')[0],
+nouveauinput  =  document.createElement("input");
+nouveauinput.setAttribute("type", "number");
+nouveauinput.setAttribute("id", ("donnc"+i));
+nouveauinput.setAttribute("value", donnc[i]);
+nouveauinput.setAttribute("style", "display: none;");
+// ajoute l'élément qui vient d'être créé et son contenu au DOM
+body.insertBefore(nouveauinput,body.childNodes[0]);
+}  
+ajouteElemen3();
 }
